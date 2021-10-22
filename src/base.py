cfc994335e3b49ad8a29de8a865d3c549ee246ad
Miaokun Chen <mc5091@columbia.edu>
2021-10-22 10:49:58 -0400

    Refactor RTCM output config on UART1

--- src/base.py
+++ src/base.py
@@ -3,10 +3,9 @@ import logging
 from serial import Serial
 from config import loadConfig
 from ublox import UBloxManager
-from pyubx2 import UBXReader
-from ubx import UBXManager
 
 log = logging.getLogger('base')
+
 
 def main():
     # load base station config
@@ -19,12 +18,11 @@ def main():
     DEBUG = bool(config["DEBUG"])
     BAUDRATE = int(config["BAUDRATE"])
     TIMEOUT = int(config["TIMEOUT"])  
+    STREAM_TTL = int(config["STREAM_TTL"])  
     PORT = config["PORT"]
     TMODE = config["TMODE"]
     OBSERVATION_TIME = int(config["OBSERVATION_TIME"])
     POSITION_ACCURACY = float(config["POSITION_ACCURACY"])
-    UART1OUTPUT_ENABLE = int(config["UART1OUTPUT_ENABLE"])
-    UART1OUTPUT_DISABLE = int(config["UART1OUTPUT_DISABLE"])
 
     # connect to serial port
     log.info(f'Connecting to serial port {PORT}')
@@ -32,7 +30,7 @@ def main():
 
     # create a ublox manager
     log.info(f'Creating UBloxManager manager on {PORT}')
-    manager = UBloxManager(stream)
+    manager = UBloxManager(stream, STREAM_TTL)
 
     # change operation mode 
     log.info(f'Setting {PORT} in {TMODE} mode')
@@ -45,29 +43,10 @@ def main():
     log.info("current TMODE3 config:")
     log.info(manager.TMODE3.getConfig())
 
-    # change UART1 output config
-    log.info("Enable UART1 RTCM output")
-    manager.UART1.configRTCMOutput(UART1OUTPUT_ENABLE)
-
-    log.info("Enable UART1 UBX output")
-    manager.UART1.configUBXOutput(UART1OUTPUT_ENABLE)
-
-    log.info("Disable UART1 NMEA output")
-    manager.UART1.configNMEAOutput(UART1OUTPUT_DISABLE)
-
-    # For each message and port a separate output rate (per second, per epoch) can be configured.
-    log.info("Set UART1 RTCM1500 output rate")
-    output_rate = 255 # output_rate should be between 0 to 255
-    manager.UART1.configRTCM_1500_OutputRate(output_rate)
-    
-    log.info("UART1 output config")
-    defaultConfig = manager.UART1.getConfig()
-    log.info(defaultConfig)
-
+    # manually get nmea stream
+    nmea_stream = manager.getNMEAStream()
     while True:
-        c = stream.read()
-        print(c.hex())
-
+        print(nmea_stream.readline())
 
 
 if __name__ == '__main__':
